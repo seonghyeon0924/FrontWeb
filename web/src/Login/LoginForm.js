22ebc7e3b4e34185b6ea7f8521daabbccc26823c
DESKTOP-UV3GQBB\PC <tjdgus0392@naver.com>
2021-05-24 23:36:47 +0900

    fake server 공부

--- web/src/Login/LoginForm.js
+++ web/src/Login/LoginForm.js
@@ -1,55 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './LoginForm.css';
+import axios from 'axios';
 // import React, {useState} from 'react'
 // import { useDispatch } from 'react-redux'
 
 
 
-export default function LoginForm(props) {
+export default function LoginForm() {
+    const [inputId, setInputId] = useState('')
+    const [inputPw, setInputPw] = useState('')
 
-    // const dispatch = useDispatch();
-    // const [Id, setId] = useState("");
-    // const [Password, setPassword] = useState("");
+        // input 데이터의 변화가 있을 때마다 value 값을 변경해서 useState해줌
+    const handleInputId = (e) => {
+        setInputId(e.target.value)
+    }
 
-//typing 하면 onChange 를 발생시켜 state를 바꿔주어 value를 바꾼다.
+    const handleInputPw = (e) => {
+        setInputPw(e.target.value)
+    }
 
+    const onClickLogin = () => {
+        console.log('click login')
+    }
 
-    // const onIdHandler = (event) => {
-    //     setId(event.currentTarget.value);
-    // }
+    //axios.get(url,[,config])  서버에서 데이터를 가져와서 보여주거나 하는 용도
+    // ex)axios.get('https://my-json-server.typicode.com/zofqofhtltm8015/fs/user').then((Response)=>{
+    //     console.log(Response.data);
+    // }).catch((Error)=>{
+    //     console.log(Error);
+    // })
+    
 
-    // const onPasswordHandler = (event) => {
-    //     setPassword(event.currentTarget.value);
-    // }
-
-    // const onSubmitHandler = (event) => {
-    //     // page refresh를 막아준다
-    //     event.preventDefault();
-
-    //     console.log("ID",Id);
-    //     console.log("Password",Password);
-
-        // 서버에 보내기
-    //     let body = {
-    //         id : Id,
-    //         password : Password
-    //     }
-
-    //     //디스패치로 액션 취하기
-    //     dispatch(loginUser(body))
-    //     .then(response => {
-    //         if (response.payload.loginSuccess) {
-    //             props.history.push('/')
-    //             //리액트에서 페이지 이동하기 위해서는 props.history.push() 이용.
-    //            	// 로그인 완료된 후 처음 화면(루트 페이지-landingpage로)으로 돌악가게 하기 
-    //         } else {
-    //             alert("Error")
-    //         }
-    //     })
-
-    // }
+    // //axios.post("url주소", {
+    //     data객체
+    // },[,config])  새로운 리소스를 create하는데 사용
+    // 로그인, 회원가입 등 사용자가 생성한 파일을 서버에다가 업로드할때 사용. post사용 시 주소창에 쿼리스트링이 남지 않으므로 get보다 안전
 
 
+    // useEffect(() => {
+    //     axios.get()
+    // })
+
+
+    const login = async () =>
+    {
+        axios.get("https://f904adec-09cc-4aec-b2e3-db9aceeb3abf.mock.pstmn.io??id="+tjdgus+"&pw="+tjdgus)
+            .then(() =>
+            {
+                console.log("Login 성공!");
+                setInputId('');
+                setInputPw('');
+            });
+    };
 
     return (
         <>
@@ -61,9 +63,9 @@ export default function LoginForm(props) {
             </div>
             <div className="login-form">
                 <form>
-                <input type="text" name="email" class="text-field" placeholder="아이디"></input>
-                <input type="password" name="password" class="text-field" placeholder="비밀번호"></input>
-                <input type="submit" value="로그인" class="submit-btn"></input>
+                <input type="text" value={inputId} name="email" class="text-field" placeholder="아이디" onChange={handleInputId}></input>
+                <input type="password" value={inputPw} name="password" class="text-field" placeholder="비밀번호" onChange={handleInputPw}></input>
+                <input type="submit" value="로그인" class="submit-btn" onClick={onClickLogin}></input>
                 </form>
 
                 <div className="links">
